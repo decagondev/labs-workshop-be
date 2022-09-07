bd99828608e687f7dbe4d1e5dacc2c6246d00f87
Tom Tarpey <tomtarpeydev@gmail.com>
2022-09-07 21:31:34 +0100

    first commit

---
+++ src/test/java/com/decadev/BeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.decadev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
